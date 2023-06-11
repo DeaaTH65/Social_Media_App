05212e849c23aee487d85de092f0d3b64ea30412
Deaath65 <deaath65@gmail.com>
2023-06-11 19:52:28 +0545

    user's profile page

--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('settings/', views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
     path('like-post', views.like_post, name='like-post'),
+    path('profile/<str:pk>', views.profile, name='profile'),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
     path('logout/', views.logout, name='logout'),
--- social_book/core/views.py
+++ social_book/core/views.py
@@ -54,6 +54,23 @@ def like_post(request):
 
 
 @login_required(login_url='signin')
+def profile(request, pk):
+    user_object = User.objects.get(username=pk)
+    user_profile = Profile.objects.get(user=user_object)
+    user_posts = Post.objects.filter(user=pk)
+    user_post_length = len(user_posts)
+
+    context = {
+        'user_object': user_object,
+        'user_profile': user_profile,
+        'user_posts': user_posts,
+        'user_post_length': user_post_length,
+        
+    }
+    return render(request, 'profile.html', context)
+
+
+@login_required(login_url='signin')
 def settings(request):
     user_profile = Profile.objects.get(user=request.user)
 
