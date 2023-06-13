d9c05010bc6c6870c454d3b1e750cc6159c42805
Deaath65 <deaath65@gmail.com>
2023-06-13 12:02:30 +0545

    people to follow

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Profile, Post, LikePost, FollowersCount
 from itertools import chain
+import random
 
 
 # Create your views here.
@@ -13,6 +14,7 @@ def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
 
+    # Show feed of followed users only
     user_following_list = []
     feed = []
 
@@ -26,9 +28,34 @@ def index(request):
         feed.append(feed_lists)
 
     feed_list = list(chain(*feed))
+
+    # User to follow
+    all_users = User.objects.all()
+    user_following_all = []
+
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
     
-    posts = Post.objects.all()
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts' : feed_list})
+    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))]
+    current_user = User.objects.filter(username=request.user.username)
+    final_suggestions_list = [x for x in list(new_suggestions_list) if ( x not in list(current_user))]
+    random.shuffle(final_suggestions_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
+
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+
+
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts':feed_list, 'suggestions_username_profile_list': suggestions_username_profile_list[:4]})
 
 
 @login_required(login_url='signin')
